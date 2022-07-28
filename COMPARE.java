e0b58356787f7226ffa069c915dce3fe4f5b0078
nikhilstacks <nikhilstacks@gmail.com>
2022-07-28 15:35:38 +0530

    checking functionality of compare to

---
+++ COMPARE.java
@@ -0,0 +1,10 @@
+
+public class COMPARE {
+    public static void main(String[] args) {
+        String name = "Nikhil";
+        String badName = "Mikhil";
+
+        int YoYo = name.compareTo(badName);
+        System.out.println(YoYo);
+    }
+}

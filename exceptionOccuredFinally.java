0c3baacffbbb8df0f297b6fabf4d14fbcd8c323a
nikhilstacks <nikhilstacks@gmail.com>
2022-07-29 12:26:42 +0530

    removing package name

--- exceptionOccuredFinally.java
+++ exceptionOccuredFinally.java
@@ -1,22 +1,19 @@
-package finalException;
-public class exceptionOccuredFinally {
-	public static void main(String[] args) {
-		try {
-			int data = 25/0;
-			System.out.println(data);
-			int[] arr = null;
-			System.out.println(arr);
-		}
-		catch(NullPointerException e){
-			System.out.println(e);
-		}
-		catch(ArithmeticException e){
-			System.out.println(e);
-		}
-		finally {
-			System.out.println("This is inside the final block...");
-		}
 
-		System.out.println("rest of the code...");
-	}
+public class exceptionOccuredFinally {
+    public static void main(String[] args) {
+        try {
+            int data = 25 / 0;
+            System.out.println(data);
+            int[] arr = null;
+            System.out.println(arr);
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("This is inside the final block...");
+        }
+
+        System.out.println("rest of the code...");
+    }
 }

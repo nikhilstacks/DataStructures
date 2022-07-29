c8db4d7e3c5814817016d68ef41732d39397277f
nikhilstacks <nikhilstacks@gmail.com>
2022-07-29 12:32:24 +0530

    return in finally and try

---
+++ TryReturn.java
@@ -0,0 +1,25 @@
+
+public class TryReturn {
+    public static void main(String[] args) {
+        try {
+            System.out.println(check());
+        } catch (Exception x) {
+            x.printStackTrace();
+
+            System.out.println("rest of the code...");
+        }
+    }
+
+    private static int check() {
+        try {
+            int res = 10 / 2;
+            if (res == 5) {
+                System.out.println("done...");
+                return res;
+            }
+        } finally {
+            System.out.println("finally");
+            return 10;
+        }
+    }
+}

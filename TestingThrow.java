097c9d73a81e0643495ceb38e37b426012b53418
nikhilstacks <nikhilstacks@gmail.com>
2022-07-29 12:31:36 +0530

    throw

---
+++ TestingThrow.java
@@ -0,0 +1,15 @@
+
+public class TestingThrow {
+    public static void validate(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("enter the valid value...");
+        } else {
+            System.out.println("Entered value is valid...");
+        }
+    }
+
+    public static void main(String[] args) {
+        validate(13);
+        System.out.println("rest of the code...");
+    }
+}

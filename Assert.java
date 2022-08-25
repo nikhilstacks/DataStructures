f3d5d517764bf5e60b10955a8285deceec4e1d9f
nikhilstacks <nikhilstacks@gmail.com>
2022-08-25 15:33:40 +0530

    assertion in java

---
+++ Assert.java
@@ -0,0 +1,8 @@
+
+public class Assert {
+    public static void main(String[] args) {
+        int a = 14;
+        assert a <= 18 : "can vote";
+        System.out.println("it is not adult: " + a);
+    }
+}

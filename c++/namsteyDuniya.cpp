59f14423c290af4a0eb69e4a4d62b9f4d2d2839c
nikhilstacks <nikhilstacks@gmail.com>
2022-08-31 01:07:08 +0530

    starting cpp

---
+++ c++/namsteyDuniya.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+    cout << "This is testing first code in vs studio code" << endl;
+    //    this is gonna be pretty interesting
+	int a = 2;
+	char b = a;
+	cout << b << endl << "agar ye chal gaya to aaj bhaag khul jaaenge...";
+	}

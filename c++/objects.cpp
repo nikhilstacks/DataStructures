9b91495b4cbd9abdbfd0a50a3225c510eeacc519
nikhilstacks <nikhilstacks@gmail.com>
2022-09-30 01:03:27 +0530

    membership label

--- c++/objects.cpp
+++ c++/objects.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
-//using namespace std;
+using namespace std;
+//----------------this is the first type method is inside the class----------------
+//class Complex
+//{
+//	int a, b;
+//    public:
+//	void setData(int x, int y){
+//		a=x;
+//		b=y;
+//	} 
+//	void  displayData(){
+//		std::cout<< "value of a is: "<< a<< " "<< "value of b is: "<<b<< std::endl;
+//	}
+//};
+
+
+//-------------this is the second type mehtod of the class is outside------------------
+
 class Complex
 {
 	int a, b;
     public:
-	void setData(int x, int y){
-		a=x;
-		b=y;
-	} 
+	void setData(int, int);
 	void  displayData(){
 		std::cout<< "value of a is: "<< a<< " "<< "value of b is: "<<b<< std::endl;
 	}
 };
+
+//---------------Complex:: This is called membership label---------------------------
+void Complex::setData(int x, int y){
+	a=x;
+	b=y;
+}
 
 main(){
 	Complex c;

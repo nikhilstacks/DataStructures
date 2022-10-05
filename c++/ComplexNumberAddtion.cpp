b264e1fdf7ddaefe1bc33ec7069fd6774ea586b5
nikhilstacks <nikhilstacks@gmail.com>
2022-10-05 22:37:10 +0530

    complex number cpp

---
+++ c++/ComplexNumberAddtion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>;
+using namespace std;
+
+class Complex{
+	private:
+		int a,b;
+	public:
+		void setData(int x, int y)
+		{
+			a=x; b=y;	
+		}
+		
+		void showData(){
+			cout<< "value of a: "<< a<< " value of b: "<<b<<endl;
+		}
+		
+		Complex add(Complex c){
+			Complex temp;
+			temp.a = a + c.a;
+			temp.b = b + c.b;
+			return temp;
+		}
+};
+
+int main()
+{
+	Complex c1, c2, c3;
+	c1.setData(3,4);
+	c2.setData(2,6);
+	c3 = c1.add(c2);
+	c3.showData();	
+}

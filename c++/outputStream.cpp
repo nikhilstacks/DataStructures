fb6e4bb9de1ebaa97171c0104e5c1d0075c76d93
nikhilstacks <nikhilstacks@gmail.com>
2022-10-09 03:53:16 +0530

    adding comments

--- c++/outputStream.cpp
+++ c++/outputStream.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 int main()
 {
-    ofstream fout;
-    fout.open("myData.dat");
+    ofstream fout; // ofstream will create a output stream from program to file
+    // if file is not created open function will create a new file
+    fout.open("myData.dat"); // if file is created previously it will overwrite the changes
     fout << "hello" << endl;
     fout << 5000;
     fout.close();

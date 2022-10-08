43d801f1abe9edcc49032db4c46b52b2d3b36495
nikhilstacks <nikhilstacks@gmail.com>
2022-10-08 21:29:51 +0530

    this value is same as the adress of the caller object

--- c++/this.cpp
+++ c++/this.cpp
@@ -10,6 +10,7 @@ public:
     void setData(int x, int y, int z)
     {
         l = x, b = y, h = z;
+        cout << "value of this " << this;
     }
     void showData()
     {
@@ -21,6 +22,7 @@ int main()
 {
     Box *p, smallBox;
     p = &smallBox;
-    p->setData(10, 12, 5);
-    p->showData();
+    cout << "value of p " << p << endl;
+    smallBox.setData(10, 12, 5);
+    smallBox.showData();
 }
